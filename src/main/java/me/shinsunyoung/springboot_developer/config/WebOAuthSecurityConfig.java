d11b33242604481e88b90ff678ee538c0d75203c
BSHkr <tmdgh784@naver.com>
2024-07-21 20:39:07 +0900

    ci.yml > cicd.yml

--- src/main/java/me/shinsunyoung/springboot_developer/config/WebOAuthSecurityConfig.java
+++ src/main/java/me/shinsunyoung/springboot_developer/config/WebOAuthSecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
